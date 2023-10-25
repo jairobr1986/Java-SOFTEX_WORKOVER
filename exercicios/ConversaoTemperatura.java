3f8598bcce59c72aff1aeb394a4a5e2f6d45ffcc
jairobr1986 <jairobr1986@gmail.com>
2023-10-25 07:52:52 -0300

    ConversaoTemperatura em java

---
+++ exercicios/ConversaoTemperatura.java
@@ -0,0 +1,24 @@
+package exercicios;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class ConversaoTemperatura {
+
+    public static void main(String[] args) {
+        // variáveis
+        double c,f;
+        // objetos
+        Scanner teclado = new Scanner(System.in);
+        DecimalFormat formatador = new DecimalFormat("#0.0");
+        // Entrada
+        System.out.println("Conversão de temperatura. ");
+        System.out.println("Digite a temperatura em fahrenheit: ");
+        f = teclado.nextDouble();
+        // processamento
+        c = (5* (f - 32)) / 9;
+        // saída
+        System.out.println("Temperatura em Celsius: " + formatador.format(c) + "°C");
+        teclado.close();
+    }
+}
